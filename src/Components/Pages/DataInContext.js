2caa61763c6b8d2525c279ebc42a62df903cef03
SGRAVANI <shakuntla.ravani@gmail.com>
2022-12-10 00:10:02 +0530

    first commit

--- src/Components/Pages/DataInContext.js
+++ src/Components/Pages/DataInContext.js
@@ -1,16 +1,44 @@
 import ContextData from "./ContextData";
 import React from 'react'
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 export default function DataInContext(props) {
     let[likeCount,setLikeCount]=useState(0)
+    let [userData,setUserData]=useState(null);
+    let [load,setLoad]=useState(false);
     function incrementLikes()
     {
         setLikeCount(likeCount+1);
     }
+    useEffect(() => {
+    setLoad(true);
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((r)=>{
+     
+     return r.json();
+     
+    })
+    .then((data)=>{
+      setUserData(data);
+      setLoad(false);
+    })
+    
+    }, [])
+    function loading()
+    {
+      if(load)
+      {
+      return (<div style={{height:"100vh"}} className=" d-flex justify-content-center align-items-center">
+        <div>Loading...</div></div>)
+      }
+    
+    else {
+      return props.children;
+    }
+  }
   return (
-    <ContextData.Provider value={{likeCount,incrementLikes}}>
-        {props.children}
+    <ContextData.Provider value={{likeCount,incrementLikes,userData,load}}>
+        {loading()}
     </ContextData.Provider>
   )
 }
--- src/Components/Pages/UserDetails.js
+++ src/Components/Pages/UserDetails.js
@@ -4,13 +4,13 @@ import {BsHeart } from "react-icons/bs";
 import { useContext } from "react";
 import ContextData from "./ContextData";
 // import User from "./User";
-let userData=[
+// let userData=[
 
-{id:"1", name:"shakuntla ravani"},
-{id:"2",name:"kartik pandiya"},
-{id:"3" ,name:"parchi patel"}
+// {id:"1", name:"shakuntla ravani"},
+// {id:"2",name:"kartik pandiya"},
+// {id:"3" ,name:"parchi patel"}
 
-];
+// ];
 function UserDetails()
 {
     let cont=useContext(ContextData);
@@ -25,19 +25,22 @@ console.log("params is : ",params)
 // useEffect(()=>{setSearchP({color:"pink",qty:200})},[])
 function getNames()
 {
-let data= userData.filter((ob)=>ob.id===params.userId)
+let data= cont.userData.find((ob)=>ob.id.toString()===params.userId)
 if(data.length==0||!data)
 {
     return "No user Found";
 }   
 else{
-    return data[0].name;
+    return data;
 }
 }
+
+const user=getNames()
 return(
     <div className="bg-light p-5 mt-5">
     <h4>User Details</h4>
-         UserName : {getNames()} 
+         UserName : {user.name} <br/>
+         Email Address: {user.email} 
           {/* <div>Search Param - Color :{searchp.get("color")}</div>
          <div>Search Param - Qty :{searchp.get("qty")}</div>  */}
          <div>
